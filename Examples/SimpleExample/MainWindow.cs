8155397cd5990ee06fd6cce19146c0e322b89584
Bert Bruggeman <b_bruggeman@ziggo.nl>
2019-04-25 06:02:41 +0200

    Media.Native&more.. big backup

---
+++ Examples/SimpleExample/MainWindow.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using BaseLib.Media.Display;
+using BaseLib.Media.OpenTK;
+using BaseLib.Xwt;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using Xwt;
+
+namespace SimpleExample
+{
+    public class MainWindow : Window
+    {
+        private Canvas3D Canvas => this.Content as Canvas3D;
+
+        public IRendererFactory Renderfactory { get; }
+        public IXwtRender XwtRender { get; }
+        public IXwt Xwt { get; }
+
+        class Canvas3D : Canvas, IRenderOwner
+        {
+            [StructLayout(LayoutKind.Explicit, Size = 4 * 3, CharSet = CharSet.Ansi)]
+            struct vertex
+            {
+                public vertex(Vector3 pos)
+                {
+                    this.pos = pos;
+                }
+                [FieldOffset(0)]
+                public Vector3 pos;
+            }
+
+            private readonly IRendererFactory RenderFactory;
+            private readonly IXwtRender XwtRender;
+            private readonly IXwt Xwt;
+
+            private IRenderer Renderer;
+            private vertices<vertex> vertices;
+            private shader shader;
+
+            private int test;
+
+            public Canvas3D(MainWindow window)
+            {
+                this.RenderFactory = window.Renderfactory;
+                this.XwtRender = window.XwtRender;
+                this.Xwt = window.Xwt;
+            }
+            internal void OnLoaded()
+            {
+                this.Renderer = this.RenderFactory.Open(this.XwtRender, this, this, new size(1920, 1080));
+
+                using (var lck = this.Renderer.GetDrawLock())
+                {
+
+                    /*    List<Vector3> simpleVertices = new List<Vector3>();
+                        simpleVertices.Add(new Vector3(0, 0, 0));
+                        simpleVertices.Add(new Vector3(100, 0, 0));
+                        simpleVertices.Add(new Vector3(100, 100, 0));*/
+
+                    this.vertices = new vertices<vertex>(
+                        new vertex[] { new vertex(new Vector3(0, -1, 0)), new vertex(new Vector3(-1, 1, 0)), new vertex(new Vector3(1, 1, 0)) });
+
+                    vertices.define("position", "pos");
+
+                    this.shader = new shader(
+        @"#version 150 core
+
+in vec4 position;
+void main()
+{
+gl_Position = position;
+}",
+         @"#version 150 core
+precision mediump float;
+
+out vec4 outColor;
+
+void main()
+{
+    outColor = vec4(1,0,0,1);
+}
+",
+                             this.vertices);
+                }
+                this.Renderer.Start();
+            }
+
+            internal void OnUnloading()
+            {
+                if (this.Renderer != null)
+                {
+                    this.Renderer.Stop();
+                    using (var lck = this.Renderer.GetDrawLock())
+                    {
+                        this.shader?.Dispose();
+                        this.vertices?.Dispose();
+                    }
+                    this.Renderer.Dispose();
+                    this.Renderer = null;
+                }
+                }
+
+            void IRenderOwner.DoEvents()
+            {
+                this.Xwt.DoEvents();
+            }
+            void IRenderOwner.EndRender(IRenderer renderer)
+            {
+                this.XwtRender.EndRender(renderer, this);
+            }
+            bool IRenderOwner.preparerender(IRenderFrame destination, bool dowait)
+            {
+                return true;
+            }
+            void IRenderOwner.render(IRenderFrame destination, Rectangle r)
+            {
+                var state = this.Renderer.StartRender(destination, r);
+
+                var c = (test++ % 25) / 25f;
+                var cc = new Xwt.Drawing.Color(c, c, c, 255);
+
+                GL.ClearColor((float)cc.Red, (float)cc.Green, (float)cc.Blue, (float)cc.Alpha);
+                GL.Clear(ClearBufferMask.ColorBufferBit/*ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit*/); // We're not using stencil buffer so why bother with clearing?            
+
+                this.vertices.Apply(this.shader);
+
+                GL.DrawArrays(PrimitiveType.Triangles, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+                GL.DisableVertexAttribArray(0);
+
+                this.Renderer.EndRender(state);
+
+                this.Renderer.Present(destination, r, IntPtr.Zero);
+            }
+            void IRenderOwner.StartRender(IRenderer renderer)
+            {
+                this.XwtRender.StartRender(renderer, this);
+            }
+        }
+        public MainWindow(IRendererFactory renderfactory, IXwtRender xwtrender, IXwt xwt)
+        {
+            this.Renderfactory = renderfactory;
+            this.XwtRender = xwtrender;
+            this.Xwt = xwt;
+
+            this.Content = new Canvas3D(this)
+            {
+                MinWidth = 100,
+                MinHeight = 100,
+                HorizontalPlacement = WidgetPlacement.Fill,
+                VerticalPlacement = WidgetPlacement.Fill,
+                ExpandHorizontal = true,
+                ExpandVertical = true
+            };
+        }
+        protected override void OnShown()
+        {
+            base.OnShown();
+
+            this.Canvas.OnLoaded();
+        }
+        protected override bool OnCloseRequested()
+        {
+            this.Canvas.OnUnloading();
+            return true;// base.OnCloseRequested();
+        }
+        protected override void OnClosed()
+        {
+            base.OnClosed();
+            Application.Exit();
+        }
+    }
+}
---
+++ Examples/SimpleExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using BaseLib.Media.Display;
+using BaseLib.Media.OpenTK;
+using BaseLib.Xwt;
+using System;
+using System.Linq;
+using Xwt;
+
+namespace SimpleExample
+{
+    static class Program
+    {
+        public static IRendererFactory RenderFactory { get; private set; }
+        public static ToolkitType ToolkitType { get; private set; }
+        public static IXwt Xwt { get; private set; }
+        public static IXwtRender XwtRender { get; private set; }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // load engine
+            try
+            {
+#if (__MACOS__)
+                TryLoad(ToolkitType.XamMac);
+#else
+                if (System.Environment.OSVersion.Platform == PlatformID.Unix || System.Environment.OSVersion.Platform == PlatformID.MacOSX)
+                {
+                    TryLoad(args.Contains("gtk3") ? ToolkitType.Gtk3 : ToolkitType.Gtk);
+                }
+                else // assume windows
+                {
+                    if (args.Contains("gtk"))
+                    {
+                        try { TryLoad(ToolkitType.Gtk); }// i386 only 
+                        catch { TryLoad(ToolkitType.Wpf); }
+                    }
+                    else { TryLoad(ToolkitType.Wpf); }
+                }
+#endif
+            }
+            catch (Exception e) { Console.Error.WriteLine($"Error initializing/loading engine '{e.Message}'"); }
+
+            // initialize Xwt (with dll load for ubuntu with both gtk2 and gtk3 installed)
+            try { BaseLib.Xwt.Platform.Initialize(Program.ToolkitType); }
+            catch (Exception e) { Console.Error.WriteLine($"Error initializing/loading xwt '{e.Message}'"); }
+
+            // intitialize xwt helpers
+            try { Program.Xwt = BaseLib.Xwt.XwtImpl.Create(); }
+            catch (Exception e) { Console.Error.WriteLine($"Error initializing/loading xwt-platform-specific '{e.Message}'"); }
+
+            // createwdinow
+
+            var window = new MainWindow(RenderFactory, XwtRender, Xwt) { Width = 250, Height = 250, Title = "Triangle" };
+            window.Show();
+            Application.Run();
+        }
+
+        private static void TryLoad(ToolkitType type)
+        {
+            ToolkitType = type;
+            XwtRender = BaseLib.Media.OpenTK.Platform.TryLoad(type, out IRendererFactory renderfactory);
+            Program.RenderFactory = renderfactory;
+        }
+    }
+}
